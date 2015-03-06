c2f54792101c97adfdbbc0f3db6aae6c93ad471a
xehartnort <xehartnort@xehartnort-HP-Pavilion-Sleekbook-15>
2015-03-06 13:29:12 +0100

    Clase BadConsequence creada.

---
+++ RubyApplication1/lib/BadConsequence.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#enconding: utf-8
+
+class BadConsequence
+  def initialize(text, levels, nVisibleTreasures, nHiddenTreasures, death)
+  @text = text
+  @levels = levels
+  @nVisibleTreasures = nVisibleTreasures
+  @nHiddenTreasuress = nHiddenTreasures
+  @death = death;
+end
