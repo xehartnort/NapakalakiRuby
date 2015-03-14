a4e4f70ee0ef9e5518be021f09bf869661ef8afa
xehartnort <xehartnort@xehartnort-HP-Pavilion-Sleekbook-15>
2015-03-14 15:04:24 +0100

    Main terminado y testeado, todo funcional y sin fallos por ahora.

--- RubyApplication1/lib/PruebaNapakalaki.rb
+++ RubyApplication1/lib/PruebaNapakalaki.rb
@@ -15,30 +15,58 @@ require_relative 'Cartas.rb'         #Solo se realiza una vez aunque lo escribas
 
 class PruebaNapakalaki
   def self.main()
-    malaCosa = BadConsequence.newDeath("Es telible")
-    print malaCosa # en lugar de poner puts malaCosa.to_s, con print ya se invoca a este método
+    cartitas=Cartas.new(10) #maximo número de objetos 10
+#    puts "Todos los monstros con un nivel de combate > 10 "
+#    PruebaNapakalaki.combatLevelGreaterThanTen(cartitas.monstruos)
+#    
+#    puts "Tdos los monstros que quitan una mano visible u oculta"
+#    PruebaNapakalaki.onlyONEHAND(cartitas.monstruos)
+#    
+#    puts "Su buen rollo indique una ganancia de niveles superior a 1"
+#    PruebaNapakalaki.levelPrizeGreatterThanOne(cartitas.monstruos)
+#
+    puts "Solo tiene por mal rollo la perdida de niveles"
+    PruebaNapakalaki.onlyLevels(cartitas.monstruos)
+    
   end
   
   #Mostrar todos los monstruos que tengas un nivel de combate superior a 10
-  def combatLevelGreaterThanTen(monsterArray)
-    resultado = Array.new  
+  def self.combatLevelGreaterThanTen(monsterArray)
     monsterArray.each do |m| 
-        if(m.combatLevel < 10)
-          resultado.push(m)
+        if(m.combatLevel > 10)
+          print m
         end
       end
-    return resultado
   end
   
   #Mostrar todos los monstruos que tengan un mal rollo que implique sólo pérdida de niveles
-#  def onlyLevels(monsterArray)
-#    resultado = Array.new
-#    monsterArray.each do |m|
-#      m.bc. 
-#    end
-#    return resultado
-#  end
-    
+  def self.onlyLevels(monsterArray)
+    monsterArray.each do |m|
+      if m.bc.death==false && 
+         (m.bc.specificVisibleTreasures.empty? && m.bc.specificHiddenTreasures.empty?) && 
+         ( m.bc.nVisibleTreasures==0 && m.bc.nHiddenTreasures==0)
+        print m
+      end
+    end
+  end
+#  Su buen rollo indique una ganancia de niveles superior a 1
+  def self.levelPrizeGreatterThanOne(monsterArray)
+    monsterArray.each do |m|
+      if m.prize.levels > 1
+         print m
+      end
+    end
+  end
+# Su mal rollo solo supone la perdida de un ONEHAND
+  def self.onlyONEHAND(monsterArray)
+    monsterArray.each do |m|
+      if m.bc.specificHiddenTreasures.include?(TreasureKind::ONEHAND) ||
+         m.bc.specificHiddenTreasures.include?(TreasureKind::ONEHAND)
+       print m
+      end
+    end
+  end
+  
 end
 PruebaNapakalaki.main
   
