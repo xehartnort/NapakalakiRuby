8bc24867733fa7beef12f3ee01ca26abf1360cb0
xehartnort <xehartnort@optimus>
2015-04-27 19:51:16 +0200

    Fallos ortográficos

--- RubyApplication1/lib/Player.rb
+++ RubyApplication1/lib/Player.rb
@@ -116,7 +116,7 @@ class Player
         if @level > 9 
           combate = CombatResult::WINANDWINGAME
         else
-          combate = CombatResult.WIN;
+          combate = CombatResult::WIN;
         end
 #      else
 #        if Dice.instance.nextNumber<5
@@ -152,11 +152,11 @@ class Player
   end
   
   def canMakeTreasureVisible(t)
-    @visibleTreasure << t #inserta al final del array el objeto t
+    @visibleTreasures << t #inserta al final del array el objeto t
     canI = true
 #    valido = [false, false, false, false, false, false]
     valido = Array.new(6, false) # array de 6 componentes, todas a false
-    @vivibleTreasure.each do |i|
+    @visibleTreasures.each do |i|
       case i.type
         when TreasureKind::ARMOR
           if valido[0]
