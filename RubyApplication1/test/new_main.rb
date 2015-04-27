78341e0e1d606a1239353c5e742cc1e60c80bf93
xehartnort <xehartnort@optimus>
2015-04-27 19:26:56 +0200

    Añadidos ficheros de prueba

--- RubyApplication1/test/new_main.rb
+++
@@ -1,8 +0,0 @@
-#Diferencias entre el uso de require_relative y load
-#require_relative solo actúa una vez y yata 
-#load acúa tantas veces como aparezca en el código, es como cargar un script
-
-require_relative 'main.rb'
-require_relative 'main.rb'
-require_relative 'main.rb'
-print "así"
