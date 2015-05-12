66cc3adb7d8f4cfa8d1d2fbfab7dc44042f755e7
xehartnort <xehartnort@optimus>
2015-05-12 17:54:51 +0200

    Implementación con simulación de clases abstractas

---
+++ RubyApplication1/lib/BCDeath.rb
@@ -0,0 +1,31 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class BCDeath < BadConsequence
+  
+  public_class_method :new
+  
+  def initialize(text, death)
+    super(text, 0, death)
+  end
+  
+  def isEmpty
+    @death==false
+  end
+  
+  def substractHiddenTreasure(t)
+  end
+
+  def substractHiddenTreasure(t)
+  end
+  
+  def adjustToFitTreasureLists(v, h)
+    self
+  end
+  
+  def to_s
+    @textoIntroduccion + "\n\tTexto = " + @text.to_s + "\n\tDeath = " + @death.to_s # return
+  end
+  
+end
--- RubyApplication1/lib/BadConsequence.rb
+++ RubyApplication1/lib/BadConsequence.rb
@@ -4,139 +4,36 @@ require_relative 'Treasures.rb'
 module Model
 class BadConsequence
   
-  ########## Constructores ##########
-  
-  def initialize(text, levels, nVisibleTreasures, nHiddenTreasures, specificVisibleTreasures, specificHiddenTreasures, death)
+  def initialize(text, levels, death)
+    @textoIntroduccion = "Esto es un mal rollo con el siguiente contenido: \n"
     @text = text
     @levels = levels
-    @nVisibleTreasures = nVisibleTreasures
-    @nHiddenTreasures = nHiddenTreasures
     @death = death
-    @specificVisibleTreasures = specificVisibleTreasures
-    @specificHiddenTreasures = specificHiddenTreasures
   end
   
-  def self.newNumberOfTreasures(text, levels, nVisible, nHidden) 
-    new(text, levels, nVisible, nHidden, Array.new, Array.new, false)
-  end
-  
-  def self.newDeath(text)
-    new(text, 0, 0, 0, Array.new, Array.new, true)
-  end
-  
-  def self.newSpecificTreasures(text, levels, tVisible, tHidden)
-    new(text, levels, 0, 0, tVisible, tHidden, false)
-  end
+  protected @textoIntroduccion, @text, @levels, @death
   
   private_class_method :new # Efecto Wert, privatiza el método new
   
   ########## Getters ruby ##########
   
-  attr_reader :text, :levels, :nVisibleTreasures, :nHiddenTreasures, :death, :specificHiddenTreasures, :specificVisibleTreasures 
+  attr_reader :text, :levels, :death 
     
   def kills
     @death
   end
-  
-  def substractVisibleTreasure(t)#Treasure t
-    if @nVisibleTreasures==0
-      if @specificVisibleTreasures.count(t.type)==1 #Si hay solo uno
-        @specificVisibleTreasures.delete(t.type)
-      else # si hay más de uno, sólo borramos el primero
-        i = @specificVisibleTreasures.find_index(t.type)
-        @specificVisibleTreasures.delete_at(i)
-      end
-    else
-      @nVisibleTreasures = (@nVisibleTreasures-1) < 0 ? 0 : @nVisibleTreasures-1 ;
-#     Exactamente igual que en java, if else compacto
-    end
-  end
-  
-  def substractHiddenTreasure(t)#Treasure t
-    if @nHiddenTreasures==0
-      if @specificHiddenTreasures.count(t.type)==1 #Si hay solo uno
-        @specificHiddenTreasures.delete(t.type)
-      else # si hay más de uno, sólo borramos el primero
-        i = @specificHiddenTreasures.find_index(t.type)
-        @specificHiddenTreasures.delete_at(i)
-      end
-    else
-      @nHiddenTreasures = (@nHiddenTreasures-1) < 0 ? 0 : @nHiddenTreasures-1 ;
-#     Exactamente igual que en java, if else compacto
-    end
-  end
  
   def isEmpty
-    return @levels==0 &&
-           @nVisibleTreasures==0 &&
-           @nHiddenTreasures==0 && 
-           @specificVisibleTreasures.empty? &&
-           @specificHiddenTreasures.empty? &&
-           @death==false
+    raise NotImplementedError.new
   end
 
   def adjustToFitTreasureLists(v, h)#v:Treasure[], h:Treasure[]
-    if !@specificVisibleTreasures.empty? || !@specificHiddenTreasures.empty?
-      newVisibleTreasuresBad = Array.new
-      newHiddenTreasuresBad = Array.new
-      copyOfspecificVisibleTreasures = @specificVisibleTreasures.clone
-      copyOfspecificHiddenTreasures = @specificHiddenTreasures.clone
-      
-      v.each do |t|
-        if copyOfspecificVisibleTreasures.include? t.type
-          index = copyOfspecificVisibleTreasures.index(t.type)
-          puts index
-          copyOfspecificVisibleTreasures.delete_at(index)
-          newVisibleTreasuresBad << t.type
-        end
-      end
-      
-      h.each do |t|
-        if copyOfspecificHiddenTreasures.include? t.type
-          index = copyOfspecificHiddenTreasures.index(t.type)
-          puts index
-          copyOfspecificHiddenTreasures.delete_at(index)
-          newHiddenTreasuresBad << t.type
-        end
-      end
-      BadConsequence.newSpecificTreasures(@text, 0, newVisibleTreasuresBad, newHiddenTreasuresBad) #return
-    else
-#     Número de tesoros visibles a quitar 
-      minVisibleTreasures =  @nVisibleTreasures > v.length ? v.length : @nVisibleTreasures
-#      Número de tesoros ocultos a quitar
-      minHiddenTreasures = @nHiddenTreasures > h.length ? h.length : @nHiddenTreasures
-      BadConsequence.newNumberOfTreasures(@text, 0, minVisibleTreasures, minHiddenTreasures) #return
-    end
+    raise NotImplementedError.new
   end
   
   def to_s
-    textoInicial = "\n\tTexto = " + @text.to_s + 
-         "\n\tLevels = " + @levels.to_s +
-         "\n\tNumber of Visible Treasures = " + @nVisibleTreasures.to_s +
-         "\n\tNumber of Hidden Treasures = " + @nHiddenTreasures.to_s +
-         "\n\tDeath = " + @death.to_s
-    textoArrayVisibleTreasures = " \n\tArray Specific Visible Treasures: "
-    textoArrayHiddenTreasures = " \n\tArray Specific Hidden Treasures: "
-    
-    unless @specificVisibleTreasures.empty?  
-      @specificVisibleTreasures.each do |tesoro|
-        textoArrayVisibleTreasures += (tesoro.to_s + " ")
-      end
-    else  
-      textoArrayVisibleTreasures += "No pierde ningún tesoro específico."
-    end
-    
-    unless @specificHiddenTreasures.empty?
-      @specificHiddenTreasures.each do |tesoro|
-        textoArrayHiddenTreasures += (tesoro.to_s + " ")
-      end
-    else
-      textoArrayHiddenTreasures += "No pierde ningún tesoro específico."
-    end
-    
-    textoInicial + textoArrayVisibleTreasures + textoArrayHiddenTreasures #return
+    raise NotImplementedError.new
   end
   
 end
-
 end
