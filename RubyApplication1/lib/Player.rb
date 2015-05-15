08390314e06443a1767e6eacab157074c7ea1812
xehartnort <xehartnort@optimus>
2015-05-15 12:41:49 +0200

    Merge origin/master
    Conflicts:
    	RubyApplication1/lib/Napakalaki.rb
    	RubyApplication1/lib/Player.rb

--- RubyApplication1/lib/Player.rb
+++ RubyApplication1/lib/Player.rb
@@ -18,55 +18,56 @@ module Model
 
     @@MAXHIDDENTREASURES=4
 
-  def initialize(name)
-    @level = 1
-    @name = name
-    @dead = true
-    @visibleTreasures = Array.new
-    @hiddenTreasures = Array.new
-    @pendingBadConsequence = BadConsequence.newNumberOfTreasures("Vacio", 0, 0, 0)
-    @dealer = CardDealer.instance
-  end
-  
-  def isDead
-    @dead
-  end
-  
-  def getVisibleTreasures
-    @visibleTreasures
-  end
-  
-  def getName
-    @name
-  end
-  
-  def getHiddenTreasures
-    @hiddenTreasures
-  end
-  
-  private ########################### Zona privada #############################
-  
-  def bringToLive 
-    @dead = false
-  end
-  
-  def incrementLevels(l)
-    @level+=l    
-  end
-  
-  def decrementLevels(l)
-      @level = @level-l<1 ? 1 : @level-l
-  end
-
-  def setPendingBadConsequence(b)
-    @pendingBadConsequence = b
-  end
-  
-  def die
-    @visibleTreasures.each do |t| 
-      @dealer.giveTreasureBack(t)
+    def initialize(name)
+      @level = 1
+      @name = name
+      @dead = true
+      @visibleTreasures = Array.new
+      @hiddenTreasures = Array.new
+      @pendingBadConsequence = BadConsequence.newNumberOfTreasures("Vacio", 0, 0, 0)
+      @dealer = CardDealer.instance
     end
 
+    def isDead
+      @dead
+    end
+
+    def getVisibleTreasures
+      @visibleTreasures
+    end
+
+    def getName
+      @name
+    end
+
+    def getHiddenTreasures
+      @hiddenTreasures
+    end
+
+    private ########################### Zona privada #############################
+
+    def bringToLive 
+      @dead = false
+    end
+
+    def incrementLevels(l)
+      @level+=l    
+    end
+
+    def decrementLevels(l)
+        @level = @level-l<1 ? 1 : @level-l
+    end
+
+    def setPendingBadConsequence(b)
+      @pendingBadConsequence = b
+    end
+
+    def die
+      @visibleTreasures.each do |t| 
+        @dealer.giveTreasureBack(t)
+      end
+    end
+    
     def getVisibleTreasures
       @visibleTreasures
     end
